49837e110acc63b9b83fc9756bb28919ab07606b
SUDHAKAR1202 <sudhakargmk12@gmail.com>
2024-11-27 17:06:39 +0530

    updated get route

--- server.js
+++ server.js
@@ -66,4 +66,8 @@ app.get('/files', async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send("Server is running");
+})
+
 app.listen(5000, () => console.log(`Server running on http://localhost:5000`));
